34a20402511b711589d66084243ed4c2845f152b
Matheus Nascimento De Oliveira <44337427830@info.local>
2020-02-05 17:05:07 -0300

    DataBase

--- Optus/Optus - DQL.sql
+++ Optus/Optus - DQL.sql
@@ -61,3 +61,59 @@ ORDER BY DataLancamento ASC;
 --COUNT 
 SELECT COUNT(IdAlbum) FROM Albuns;
 SELECT * FROM Albuns;
+
+--SELECIONAR OS ALBUNS DO MESMO ARTISTA
+SELECT * FROM Albuns WHERE IdMusicos = 2;
+
+-- USANDO INNER JOIN (JUNÇÃO E DUAS OU MAIS TABELAS)
+SELECT Musicos.Nome, Albuns.Nome FROM Musicos
+INNER JOIN Albuns ON Musicos.IdMusicos = Albuns.IdAlbuns
+WHERE Albuns.IdAlbuns = 2;
+
+--SELECIONAR OS ALBUNS NA MESMA DATA 
+SELECT * FROM Albuns WHERE DataLancamento = '2020-01-23';
+
+--INNNER JOIN 
+SELECT * FROM Musicos
+INNER JOIN Albuns ON Musicos.IdMusicos = Albuns.IdMusicos
+WHERE DataLancamento = '2020-01-23';
+
+-- SELECIONAR ALBUNS E ARTISTAS E ORDENAR POR DATA DE LANCAMENTO 
+--(DO MAIS ANTIGO PARA O MAIS RECENTE)
+
+--FORMA SIMPLES 
+SELECT * FROM Albuns ORDER BY DataLancamento ASC;
+
+--COM JOIN 
+SELECT Musicos.Nome AS NomeArtista, Albuns.Nome AS NomeAlbum, Albuns.DataLancamento --Atributos com Apelidos AS ...
+FROM Musicos INNER JOIN Albuns ON Musicos.IdMusicos = Albuns.IdAlbuns
+ORDER BY DataLancamento ASC;
+
+--SELECIONAR ARITISTAS DO MESMO ESTILO MUSICAL
+SELECT IdMusicos, IdEstilo FROM Albuns WHERE IdAlbuns;
+
+--DESAFIO REALIZAR EXERCÍCIO
+-- COM INNER JOIN 
+-- Aparecer nome do Artista e nome do Estilo 
+
+SELECT Musicos.Nome AS NomeArtista, Estilos.Nome AS NomeEstilo
+FROM Musicos
+INNER JOIN Albuns ON Musicos.IdMusicos = Albuns.IdMusicos
+INNER JOIN Estilos ON Albuns.IdEstilo = Estilos.IdEstilo
+WHERE Estilos.IdEstilo = 3;
+
+USE OptusTarde_DDL
+
+CREATE PROCEDURE OrdemLancamento
+AS
+SELECT * FROM Albuns
+ORDER BY Albuns.DataLancamento ASC;
+
+EXECUTE OrdemLancamento;
+
+CREATE PROCEDURE OrdemVisualizacao
+AS
+SELECT * FROM Albuns
+ORDER BY Albuns.Visualizaçao ASC;
+
+EXECUTE OrdemVisualizacao;
